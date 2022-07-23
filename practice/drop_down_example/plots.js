81385340b27c7d5e33e8ddba3cfa4e1fceb6ad8b
Tyler Hendricks <tylerhendricks@mail.weber.edu>
2022-07-23 12:27:17 -0600

    updated practice files, created new index.html

---
+++ practice/drop_down_example/plots.js
@@ -0,0 +1,19 @@
+d3.json("samples.json").then(function(data){
+    console.log(data);
+});
+
+// get the wfreq data, filtered
+d3.json("samples.json").then(function(data){
+    wfreq = data.metadata.map(person =>
+person.wfreq).sort((a,b) => b - a);
+    filteredWfreq = wfreq.filter(element => element !=
+null);
+    console.log(filteredWfreq);
+});
+
+// get the data and print for a single record
+d3.json("samples.json").then(function(data){
+    firstPerson = data.metadata[0];
+    Object.entries(firstPerson).forEach(([key, value]) =>
+      {console.log(key + ': ' + value);});
+});
